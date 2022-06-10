fda3803960f6522e4ac0c1805eeb6aaf028a7f7a
nawa2179 <nawad.hkm007@gmail.com>
2022-06-10 23:02:06 +0200

    5_Peak_calling.sh

---
+++ ATAC-seq/5_Peak_calling.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH -p workq
+#SBATCH -t
+#Load binaries
+module load bioinfo/macs-v2.2.7.1
+
+dir_bam_sorted=(/work/nhakim/4_Bowtie2_align/sorted_bam_file/)
+
+dir_out=(/home/nhakim/work/peaks-calling-pval0.01/)
+
+ext_sorted_bam_file=".sorted.bam"
+
+date
+
+for i in GFP1_S3 GFP2_S4 GFP3_S5 KO2_S1 KO3_S2
+do
+
+macs2 callpeak -t $dir_bam_sorted$i$ext_sorted_bam_file -f BAMPE -n $i -g 1.87e9 -p 0.01 --outdir $dir_out
+
+#-f format ->  bam pairend
+#-n sample's name
+#-g: lgenome length 
+#-p: pvalue
+
+done
